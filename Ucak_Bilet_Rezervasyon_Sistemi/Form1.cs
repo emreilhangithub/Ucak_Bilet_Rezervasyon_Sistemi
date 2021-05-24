a0128f1b02d5d1c2496f72522dcddcd18d86cba2
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-24 15:10:08 +0300

    ekran tasarımı tamamlandı

--- Ucak_Bilet_Rezervasyon_Sistemi/Form1.cs
+++ Ucak_Bilet_Rezervasyon_Sistemi/Form1.cs
@@ -16,5 +16,10 @@ namespace Ucak_Bilet_Rezervasyon_Sistemi
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
